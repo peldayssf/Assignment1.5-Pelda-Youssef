c45a23c529431eb642063a0f854bf6c36e2ed45d
peldayssf <ekpeldaek47@gmail.com>
2024-12-05 22:41:56 +0100

    Add files via upload

---
+++ Assignment 1.5 + Pelda Youssef/script.js
@@ -0,0 +1,19 @@
+let vip = true;
+let age = 17;
+
+
+{if (vip == true) 
+        alert("Access granted")
+    else 
+        alert ("access denied")}
+
+if (age >=18) {
+    console.log(true);
+}else {
+    console.log(false); 
+}
+    
+        
+
+    
+     
